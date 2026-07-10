2916d955d5abc4cd1da238967320e85657f12a20
Pedro <phguerson@gmail.com>
2026-07-10 11:10:17 -0300

    feat: bootstrap Spring Boot backend

---
+++ backend/src/main/java/com/pedrohenrique/cybersentinel/CybersentinelApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohenrique.cybersentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybersentinelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedrohenrique/cybersentinel/CybersentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohenrique.cybersentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
